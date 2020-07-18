27d968caa60e57ff615792e47d4812ee10807061
JackTheWebDev <fakeEmail@GetRekt.lmao>
2020-07-19 00:56:41 +1200

    ok this works. you can now set the message and the color of the player name

--- src/com/jackthewebdev/WelcomeMessage/Core.java
+++ src/com/jackthewebdev/WelcomeMessage/Core.java
@@ -9,6 +9,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 import static net.minecraft.server.v1_15_R1.Items.I;
 
 public class Core extends JavaPlugin implements Listener {
@@ -26,30 +28,35 @@ public class Core extends JavaPlugin implements Listener {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(command.getName().equalsIgnoreCase("setplayercolor")){
+        if(command.getName().equalsIgnoreCase("setjoincolor")){
             String color = args[0];
-            this.getConfig().set("color", color);
+            this.getConfig().set("color", color.toUpperCase());
             sender.sendMessage("Set the player name color to: " + ChatColor.valueOf(this.getConfig().get("color").toString()) + "this");
             return true;
         }
         if(command.getName().equalsIgnoreCase("setjoinmessage")){
             StringBuilder buffer = new StringBuilder();
-
             // change the starting i value to pick what argument to start from
             // 1 is the 2nd argument.
             for(int i = 0; i < args.length; i++)
             {
-                buffer.append(' ').append(args[i]);
+                if(!args[i].equals(" ")){
+                    buffer.append(' ').append(args[i]);
+                }
             }
-            this.getConfig().set("message",buffer.toString());
-            sender.sendMessage("Set the message to: "+this.getConfig().get("message").toString()+", "+ChatColor.valueOf(this.getConfig().get("color").toString())+"[player username]");
+            String message = buffer.toString();
+            String trimmed = message.trim();
+
+            this.getConfig().set("message",trimmed);
+            sender.sendMessage("The welcome message will look like this: "+this.getConfig().get("message")+", "+ChatColor.valueOf(this.getConfig().get("color").toString())+"[player name here]");
             return true;
         }
         return false;
     }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         Player p = event.getPlayer();
-        p.sendMessage( this.getConfig().get("message")+", "+ChatColor.valueOf(this.getConfig().get("color").toString())+p.getName().toString());
+        p.sendMessage( this.getConfig().get("message")+", "+ChatColor.valueOf(Objects.requireNonNull(this.getConfig().get("color")).toString())+p.getName().toString());
     }
 }
